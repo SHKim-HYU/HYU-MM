09ea0080c661119abe4778fe8ee10531b72dabc0
SHKim-HYU <tjsghd101@naver.com>
2026-06-01 20:02:23 +0900

    Remove committed catkin build/ artifacts (rplidar_ros, ira_laser_tools); gitignore build dirs

--- ira_laser_tools/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robot/ros_ws/catkin_ws/src/HYU-MM/ira_laser_tools
-# Build directory: /home/robot/ros_ws/catkin_ws/src/HYU-MM/ira_laser_tools/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
